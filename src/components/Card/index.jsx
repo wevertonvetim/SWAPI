77b9d94d8e72288c8e583d8789a92848e78d5673
frattezi <pfrattezi@gmail.com>
2021-10-07 17:49:26 -0300

    fix: card to use variables

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import './estilo.css';
 
-function Card() {
+function Card({ title, name, infoText }) {
   return (
-    <header className="card">
-      <div className="container__card">
+    <header className="card-header">
+      <div className="card-display">
         <div className="titulo">
-          <h3 className="card__titulo">Visualizador SWAPI</h3>
+          <h3 className="card__titulo">{title}</h3>
         </div>
-        <ul className="card__lista">
-          <li className="card__lista__informacoes">nome</li>
-          <li className="card__lista__informacoes">informação</li>
-          <li className="card__lista__informacoes">informação</li>
-          <li className="card__lista__informacoes">informação</li>
-        </ul>
+        <div className="card-body">
+          <h2>{name}</h2>
+          <p>{infoText}</p>
+        </div>
       </div>
     </header>
   );
